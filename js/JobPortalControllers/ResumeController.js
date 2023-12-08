047910b1363cebbf7862e6f8c0a56956210d9ebc
Dark <hkhzm02@gmail.com>
2023-12-08 12:39:08 -0800

    Resume Changes

--- js/JobPortalControllers/ResumeController.js
+++ js/JobPortalControllers/ResumeController.js
@@ -1,15 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelector('.add-skill').addEventListener('click', addSkill);
+    document.querySelector('.add-project').addEventListener('click', addProject);
     document.getElementById('profileForm').addEventListener('submit', submitForm);
 });
+
 
 function addSkill() {
     const skillsContainer = document.getElementById('skillsContainer');
     const newSkillInput = document.querySelector('.skill-input').cloneNode(true);
     newSkillInput.querySelector('input').value = '';
     const addButton = newSkillInput.querySelector('button');
-    addButton.textContent = '-'; // Change button text to '-'
-    addButton.style.backgroundColor = '#dc3545'; // Change button color to red
+    addButton.textContent = '-'; 
+    addButton.style.backgroundColor = '#dc3545'; 
     addButton.addEventListener('click', removeSkill);
     skillsContainer.appendChild(newSkillInput);
 }
@@ -23,8 +25,30 @@ function removeSkill() {
     }
 }
 
+function addProject() {
+    const projectsContainer = document.getElementById('projectsContainer');
+    const newProjectInput = document.querySelector('.project-input').cloneNode(true);
+    newProjectInput.querySelector('input').value = '';
+    const addButton = newProjectInput.querySelector('button');
+    addButton.textContent = '-'; 
+    addButton.style.backgroundColor = '#dc3545'; 
+    addButton.addEventListener('click', removeProject);
+    projectsContainer.appendChild(newProjectInput);
+}
+
+function removeProject() {
+    const projectsContainer = document.getElementById('projectsContainer');
+    const projectInputs = projectsContainer.querySelectorAll('.project-input');
+    if (projectInputs.length > 1) {
+        const lastProjectInput = projectInputs[projectInputs.length - 1];
+        projectsContainer.removeChild(lastProjectInput);
+    }
+}
+
 function submitForm(event) {
     event.preventDefault();
+    
+   
     const skillsInputs = document.querySelectorAll('input[name="skills[]"]');
     const skills = Array.from(skillsInputs).map(input => input.value).filter(skill => skill.trim() !== '');
     const skillsField = document.createElement('input');
@@ -32,5 +56,16 @@ function submitForm(event) {
     skillsField.name = 'skills';
     skillsField.value = skills.join(', ');
     event.target.appendChild(skillsField);
+
+    
+    const projectsInputs = document.querySelectorAll('input[name="projects[]"]');
+    const projects = Array.from(projectsInputs).map(input => input.value).filter(project => project.trim() !== '');
+    const projectsField = document.createElement('input');
+    projectsField.type = 'hidden';
+    projectsField.name = 'projects';
+    projectsField.value = projects.join(', ');
+    event.target.appendChild(projectsField);
+
     event.target.submit();
 }
+
